36f231194cdf8d60f41821c6526ebd46f083153c
Samenal-sys <malachilasts+1@gmail.com>
2024-12-12 16:17:43 -0500

    Update num.js

--- num.js
+++ num.js
@@ -5,32 +5,42 @@ let attemptsLeft;
 function start() {
     secretNumber = Math.floor(Math.random() * 100) + 1;
     attemptsLeft = 10;
-    document.getElementById("message").innerText = "Game started. You have 10 attempts.";
+    document.getElementById("message").innerText = "Game started. You have 10 attempts. Only sigmas can win this EPIC game.";
     document.getElementById("estim").value = "";
     document.getElementById("estim").disabled = false;
     document.getElementById("guess").disabled = false;
+    const access = localStorage.getItem("lost");
+    if (access == 1) then {
+        alert("nuh uh buddy you lost the test and arent sigma");
+        window.close();
+    }
+
 }
 
 function guess() {
     let guess = parseInt(document.getElementById("estim").value);
     if (isNaN(guess) || guess < 1 || guess > 100) {
-        alert("Please enter a valid number between 1 and 100.");
+        alert("not so sigma of you please enter a real number that is between or equal to 1 and 100.");
         return;
     }
 
     attemptsLeft--;
     if (guess === secretNumber) {
-        document.getElementById("message").innerText = `Congratulations! You guessed the number ${secretNumber} in ${10 - attemptsLeft} attempts.`;
+        document.getElementById("message").innerText = `you are so sigma for getting ${secretNumber} in ${10 - attemptsLeft} attempts. + ${10 + attemptsLeft} aura.`;
         document.getElementById("estim").disabled = true;
         document.getElementById("estim").value = ""
         document.getElementById("guess").disabled = true;
     } else if (attemptsLeft === 0) {
-        document.getElementById("message").innerText = `Out of attempts. The number was ${secretNumber}.`;
+        document.getElementById("message").innerText = `Out of attempts. The number was ${secretNumber}. you failed so you arent a sigma`;
+        localStorage.setItem("lost","1");
         document.getElementById("estim").disabled = true;
         document.getElementById("estim").value = ""
         document.getElementById("guess").disabled = true;
+        alert("banned from playing");
+        window.close();
+        
     } else {
         let hint = guess < secretNumber ? "higher" : "lower";
-        document.getElementById("message").innerText = `Incorrect. Try a ${hint} number. Attempts left: ${attemptsLeft}.`;
+        document.getElementById("message").innerText = `Incorrect. Try a ${hint} number. Attempts left: ${attemptsLeft}. dont lose or you get banned.`;
     }
 }
